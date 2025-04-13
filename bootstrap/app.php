3e14bf6d72aba4bda11d16ff6aa8dfaf7d01cd50
Miguel Angel <migueel26@gmail.com>
2025-04-13 13:06:12 +0200

    subiendo proyecto

---
+++ bootstrap/app.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        // aqui vamos a llamar los Middleware que vayamos creando
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
